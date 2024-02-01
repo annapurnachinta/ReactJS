8dcf119c9dbe1d0239fba017cc50f33e360be9ba
annapurnachinta <annapurnachinta1234@gmail.com>
2024-02-02 00:55:29 +0530

    section 8: learned about communication with context, refactor the code

---
+++ Section 8 - Communication Using the Context System/reading-books/src/components/BookCreate.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import useBooksContext from '../hooks/use-book-context'
+
+function BookCreate() {
+    const [title, setTitle] = useState('')
+    const { createBook } = useBooksContext()
+
+    const handleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        createBook(title)
+        setTitle('')
+    }
+
+    return <div className='book-create'>
+        <h3>Add a book</h3>
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input value={title} onChange={handleChange} className='input' required/>
+            <button className='button'>Create!</button>
+        </form>
+    </div>
+}
+
+export default BookCreate
---
+++ Section 8 - Communication Using the Context System/reading-books/src/components/BookList.js
@@ -0,0 +1,15 @@
+import BookShow from './BookShow'
+import useBooksContext from '../hooks/use-book-context'
+
+function BookList() {
+    const {books} = useBooksContext()
+
+    const renderedBook = books.map((book) => {
+        return <BookShow key={book.id} book={book} />
+    })
+    return <div className='book-list'>
+        {renderedBook}
+    </div>
+}
+
+export default BookList
